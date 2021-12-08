f6e3d4a9eeec1ef323c8e4850998d7940542d649
RajeshPatel17 <rajesh.patel20110@gmail.com>
2021-12-08 16:03:52 -0600

    finished day 1 part 1

---
+++ Day1/Day1.java
@@ -0,0 +1,37 @@
+package Day1;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Day1 {
+    public static void part1() throws FileNotFoundException{
+        File file = new File("Day1\\input1.txt");
+        if(!file.isFile()){
+            System.out.println("File not found");
+            return;
+        }
+        Scanner input = new Scanner(file);
+        int prev = input.nextInt();
+        input.nextLine();
+        int count = 0;
+        while(input.hasNextLine()) {
+            int next = input.nextInt();
+            input.nextLine();
+            if(prev<next){
+                count++;
+            }
+            prev = next;
+        }
+        input.close();
+        System.out.println("increase count " + count);
+    }
+
+    public static void part2(){
+
+    }
+    public static void main(String[] args) throws FileNotFoundException{
+        part1();
+
+    }
+}
